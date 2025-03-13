40e1a18b566698be8012dc56348eebd3f28bd003
Mertcan Serintürk <mserinturk@gmail.com>
2025-03-13 14:51:34 +0300

    created my first react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <div class="border">test</div>
+      Merhaba Dünya
+      <i class="fa-light fa-user"></i>
+    </>
+  )
+}
+
+export default App
